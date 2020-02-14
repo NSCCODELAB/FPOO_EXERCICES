ec6aa7e9a70b2faa913c9cce9af22572d5e89d45
NSCCODELAB <nsccodelab@gmail.com>
2020-02-14 18:44:35 -0300

    commit

---
+++ FPOO_EXERCISES/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FPOO_EXERCISES
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
